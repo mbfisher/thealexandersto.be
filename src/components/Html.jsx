80aa88ff610c0f420ca0373833b419e190d4e93a
Mike Fisher <mike@mbfisher.com>
2015-06-24 07:36:57 +0100

    Remove rogue n!

--- src/components/Html.jsx
+++ src/components/Html.jsx
@@ -10,10 +10,11 @@ export default class Html extends React.Component {
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"/>
             <link rel="stylesheet" href="/index.css"/>
+            <title>thealexandersto.be</title>
             </head>
             <body>
                 <div id="app" dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
-            </body>n
+            </body>
             <script dangerouslySetInnerHTML={{__html: 'window.App=' + this.props.state + ';'}}></script>
             <script src="https://cdn.firebase.com/js/client/2.2.6/firebase.js"></script>
             <script src="/bundle.js"></script>
